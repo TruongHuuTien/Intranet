4a6540402e7d884cdf9e567b633552d4ccaac2c8
fabish <fabien.vautour@softconceptcanada.com>
2014-01-24 14:08:07 -0500

    0.0.2

--- php/views/layout/contact.php
+++ php/views/layout/contact.php
@@ -12,7 +12,7 @@
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet/less" href="<?php echo ROOT_URL; ?>css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo ROOT_URL; ?>css/bootstrap.min.css">
         <script src="<?php echo ROOT_URL; ?>js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
