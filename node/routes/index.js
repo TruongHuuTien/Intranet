9ab305baa3a44b5ea05d180ea1703aa5599b8253
TRUONG Huu-Tien <truong.htien@gmail.com>
2014-01-24 16:16:26 -0500

    0.0.2

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,7 +6,7 @@ var colors	= require('colors');
 
 var PHP_SERVER_URL	= 'http://192.168.0.118:8888';
 
-var _dev			= true;
+var _dev			= false;
 
 /* Log all input */
 exports.log_input = function(req, res, next) {
@@ -20,7 +20,7 @@ exports.loadFile = function(req, httpRes) {
 	url = req.url;
 	filePath = path.dirname(require.main.filename)+'/html'+url;
 	if (pathParsed = url.match(/^\/(.*)(\.html)$/)) { // Convert .html -> .php
-		requestPath = '/layout.php?content='+pathParsed[1];
+		requestPath = '/layout.php?content='+pathParsed[1]+'&filePath='+filePath;
 	} else {
 		requestPath = url;
 	}
@@ -31,48 +31,46 @@ exports.loadFile = function(req, httpRes) {
 	};
 	
 	// fs.exist check the existance of the requested file
-	fs.exists(filePath, function(exists) {
-		console.log(filePath, exists);
-		if (_dev) { // server is on Developpement mode
-			if (exists) {
-				// Get file Information for cache date; stats.atime = file's Add Dates
-				stats = fs.statSync(filePath);
-				requestParam.headers = {
-					'If-None-Match'		: '*',
-					'If-Modified-Since'	: new Date(stats.atime).toUTCString()
-				};
-			}
-			//Send a request with|out cache
-			console.log(requestParam.url);
-			request(requestParam, function(err, res, buffer) {
-				if (err == null) {
-					if (res.statusCode === 304) {
+	if (_dev) { // server is on Developpement mode
+		if (fs.existsSync(filePath)) {
+			// Get file Information for cache date; stats.atime = file's Add Dates
+			stats = fs.statSync(filePath);
+			requestParam.headers = {
+				'If-None-Match'		: '*',
+				'If-Modified-Since'	: new Date(stats.atime).toUTCString()
+			};
+		}
+		//Send a request with|out cache
+		console.log(requestParam.url);
+		request(requestParam, function(err, res, buffer) {
+			if (err == null) {
+				if (res.statusCode === 304) {
+					httpRes.sendfile(filePath);
+				} else if (res.statusCode === 200) {
+					// Write file before sending
+					fs.writeFile(filePath, buffer, function(err) { 
 						httpRes.sendfile(filePath);
-					} else if (res.statusCode === 200) {
-						// Write file before sending
-						fs.writeFile(filePath, buffer, function(err) { 
-							httpRes.sendfile(filePath);
-						});
-					}
+					});
+				}
+			} else {
+				console.log('Error with path: '+requestParam.url);
+			}
+		});
+	} else { // Server is on Production Mode
+		if (!fs.existsSync(filePath)) { // We don't have this file, request it from php server
+			request(requestParam, function(err, res, buffer) {
+				console.log(res);
+				if (err == null) {
+					// Write file before sending
+					fs.writeFile(filePath, buffer, function(err) {
+						httpRes.sendfile(filePath);
+					});
 				} else {
 					console.log('Error with path: '+requestParam.url);
 				}
 			});
-		} else { // Server is on Production Mode
-			if (!exists) { // We don't have this file, request it from php server
-				request(requestParam, function(err, res, buffer) {
-					if (err == null) {
-						// Write file before sending
-						fs.writeFile(filePath, buffer, function(err) {
-							httpRes.sendfile(filePath);
-						});
-					} else {
-						console.log('Error with path: '+requestParam.url);
-					}
-				});
-			} else {
-				httpRes.sendfile(filePath);
-			}
+		} else {
+			httpRes.sendfile(filePath);
 		}
-	});
+	}
 };
