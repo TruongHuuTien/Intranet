f9681ad9392f1f3995286b88527ae9cf4fe73be7
TRUONG Huu-Tien <truong.htien@gmail.com>
2014-01-24 13:02:26 -0500

    0.0.1
    initial commit

--- php/index.php
+++ php/index.php
@@ -1,7 +1,15 @@
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0">
 	<title>Intranet</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" charset="UTF-8"></script>
+	<script type="text/javascript" charset="UTF-8">
+	$(document).ready(function() {
+		
+	});
+	</script>
 	<style type="text/css">
 	body {
 		background-image: url('main_bgr.png');
@@ -9,6 +17,8 @@
 	</style>
 </head>
 <body>
-
+	<h1>Bienvenue sur l'Intranet de Softconcept Canada</h1>
+	<h2>Fait par des développeurs web, pour nous même et tous les autres</h2>
+	<h3>À consommer sans modération</h3>
 </body>
 </html>
